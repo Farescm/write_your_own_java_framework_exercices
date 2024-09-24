973cd4c17486156765a185d966a6f59ae7741296
Fares Pc Esipe2 <fareshashmap@gmail.com>
2024-09-24 10:49:42 +0200

    Q1 + un peu Q2 Avant la liste

--- interceptor/src/main/java/org/github/forax/framework/interceptor/InterceptorRegistry.java
+++ interceptor/src/main/java/org/github/forax/framework/interceptor/InterceptorRegistry.java
@@ -7,5 +7,47 @@ import java.util.*;
 import java.util.stream.Stream;
 
 public final class InterceptorRegistry {
-  private AroundAdvice advice;
+    private AroundAdvice advice;
+
+    private final HashMap<Class<?>, AroundAdvice> adviceMap = new HashMap<>();
+
+    public void addAroundAdvice(Class<? extends Annotation> annotationClass, AroundAdvice advice) {
+        Objects.requireNonNull(annotationClass);
+        Objects.requireNonNull(advice);
+        var result = adviceMap.putIfAbsent(annotationClass, advice);
+        if (result != null) {
+            throw new IllegalStateException();
+        }
+    }
+
+    private List<AroundAdvice> findAllAdvices(Method method) {
+        return Arrays.stream(method.getAnnotations())
+                .flatMap(annotation ->
+                {
+                    var advice = adviceMap.get(annotation.annotationType());
+                    return Stream.ofNullable(advice);
+                })
+                .toList();
+    }
+
+    public <T> T createProxy(Class<T> type, T instance) {
+        Objects.requireNonNull(type);
+        Objects.requireNonNull(instance);
+        return type.cast(Proxy.newProxyInstance(type.getClassLoader(), new Class[]{type},
+                (proxy, method, args) -> {
+                    var advices = findAllAdvices(method);
+                    for (var advice : advices) {
+                        advice.before(instance, method, args);
+                    }
+                    Object result = null;
+                    try {
+                        result = Utils.invokeMethod(instance, method, args);
+                    } finally {
+                        for (var advice : advices) {
+                            advice.after(instance, method, args, result);
+                        }
+                    }
+                    return result;
+                }));
+    }
 }
